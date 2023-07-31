1f4cec415b70b7eea0db01b77897e6eed397fcb9
BillCipher-exe <hakanbaser@hotmail.de>
2023-07-31 16:15:24 +0200

    changed CLI Input packged to Terminal-Kit
    Fixed Ö Ü Ä ß Inputs

--- storage.js
+++ storage.js
@@ -1,10 +1,16 @@
 let LocalStorage = require('node-localstorage').LocalStorage;
 const Cryptr = require("cryptr");
-const prompt = require("prompt-sync")();
 const fs = require("fs");
 let table = require("table");
 localStorage = new LocalStorage('./save');
 
+//
+var term = require( 'terminal-kit' ).terminal ;
+let prompt = async ()=>
+{
+
+}
+//
 let data = {content: []};
 
 let print_data = (index) =>
@@ -59,36 +65,39 @@ let print_data = (index) =>
         }
     }
     
-    console.log(table.table(table_content, config));
+    console.log("\n"+table.table(table_content, config));
 }
    
-let write_data = () =>
-{
-    console.log("write data");
-    
+let write_data = async () =>
+{    
     let new_data={};
-    new_data.name = prompt("plattform: ");
+    term("\nPlattform: ");
+    new_data.name = await term.inputField().promise;
     if(new_data.name===null)
     {
         new_data.name="NULL";
     }
-    new_data.descr = prompt("description: ");
-    new_data.usr = prompt("username: ");
-    new_data.pw = prompt("password: ");
+    term("\ndescription: ");
+    new_data.descr = await term.inputField().promise;
+    term("\nusername: ");
+    new_data.usr = await term.inputField().promise;
+    term("\npassword: ");
+    new_data.pw = await term.inputField().promise;
     data.content.push(new_data);
 }
 
-let delete_data = () =>
+let delete_data = async () =>
 {
     print_data();
-    console.log("which record do you want to delete: ")
-    let line = prompt("Input line ID: ")
+    term("which line to delete: ");
+    let line = await term.inputField().promise;
     data.content.splice(line,1);
 }
 
-let search_data = () =>
+let search_data = async () =>
 {
-    let search = prompt("search Plattform: ");
+    term("\nsearch: ");
+    let search = await term.inputField().promise;
     let index_match=[];
     for(i in data.content)
     {
@@ -100,47 +109,57 @@ let search_data = () =>
     print_data(index_match);
 }
 
-if(!fs.existsSync("./save/data"))
-{   try
-    {
-        const key_init = prompt("set your password: ");
-        let cryptr_init = new Cryptr(key_init);
-        localStorage.setItem("data",cryptr_init.encrypt(JSON.stringify(data)));
-    }
-    catch(error)
-    {
-        console.log("Password cant be Empty. Try again.")
-    }    
-}
-else
+
+let main = async ()=>
 {
-
-    const key_string = prompt("Password: ");
-    try
-    {
-        let cryptr = new Cryptr(key_string);
-        data = JSON.parse(cryptr.decrypt(localStorage.getItem("data")));
-    
-        let input = ""
-        print_data();
-        while (input!="q")
+    if(!fs.existsSync("./save/data"))
+    {   try
         {
-            console.log("|p: print all|   |w: write|    |d: delete|  |s: search|   |q: quit|");
-            input = prompt("input: ");
-            switch(input)
-            {
-                case "p": print_data();break;
-                case "w": write_data();localStorage.setItem("data",cryptr.encrypt(JSON.stringify(data)));break;
-                case "d": delete_data();localStorage.setItem("data",cryptr.encrypt(JSON.stringify(data)));break;
-                case "s": search_data();break;
-                case "q": input = "q";break;
-                default:
-            }
+            term("SETUP Password: ");
+            const key_init =  await term.inputField().promise;
+            let cryptr_init = new Cryptr(key_init);
+            localStorage.setItem("data",cryptr_init.encrypt(JSON.stringify(data)));
+            process.exit();
         }
+        catch(error)
+        {
+            console.log("Password cant be Empty. Try again.")
+        }    
     }
-    catch(error)
+    else
     {
-        console.log("wrong password fool")
-    }    
-
+        term("Password: ");
+        const key_string =  await term.inputField().promise;
+        try
+        {
+            let cryptr = new Cryptr(key_string);
+            data = JSON.parse(cryptr.decrypt(localStorage.getItem("data")));
+        
+            let input = ""
+            print_data();
+            while (input!="q")
+            {
+                console.log("|p: print all|   |w: write|    |d: delete|  |s: search|   |q: quit|");
+                term("\Input: ");
+                input =  await term.inputField().promise;
+                switch(input)
+                {
+                    case "p": print_data();break;
+                    case "w": await write_data();localStorage.setItem("data",cryptr.encrypt(JSON.stringify(data)));break;
+                    case "d": await delete_data();localStorage.setItem("data",cryptr.encrypt(JSON.stringify(data)));break;
+                    case "s": await search_data();break;
+                    case "q": input = "q";break;
+                    default:break;
+                }
+            }
+            process.exit();
+        }
+        catch(error)
+        {
+            console.log("wrong password fool");
+            process.exit();
+        }    
+    }
 }
+
+main();
